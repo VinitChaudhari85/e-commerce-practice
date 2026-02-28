4a01d8a7a2ef23a9315751ce8884756c78725671
Vinit <chaudhari7876@gmail.com>
2026-02-28 17:04:16 +0530

    Further progress
    Further progress made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import cartImage from "../assets/cart.png"
-import { Link } from "react-router-dom"
+import cartImage from "../assets/cart.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return <header>
-        <nav className="flex justify-between py-3 px-36 items-center">
-            <p className="text-3xl font-extrabold">STORE</p>
-            <ul className="flex gap-6 items-center text-xl">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/products">Products</Link></li>
-                <li><Link to="/cart"><img src={cartImage} alt="cart-logo" className="h-9" /></Link></li>
-            </ul>
-        </nav>
+  return (
+    <header>
+      <nav className="flex justify-between py-3 px-36 items-center">
+        <p className="text-3xl font-extrabold">STORE</p>
+        <ul className="flex gap-6 items-center text-xl">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products?category=keyboards">Keyboards</Link>
+          </li>
+          <li>
+            <Link to="/products?category=mice">Mice</Link>
+          </li>
+          <li>
+            <Link to="/products?category=mousepads">Mousepads</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <img src={cartImage} alt="cart-logo" className="h-9" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-};
+  );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,26 @@
-import useFetch from "../hooks/useFetch"
+import useFetch from "../hooks/useFetch";
+import { useSearchParams } from "react-router-dom";
 
 export default function Products() {
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
 
-    const { data : products, loading, error } = useFetch('http://localhost:3001/products');
+  const {
+    data: products,
+    loading,
+    error,
+  } = useFetch("http://localhost:3001/products");
 
+  const filteredProducts = products
+    ? products.filter((product) => product.category === category)
+    : [];
 
-    return <div>
-        This is the products page.
-        { products && products.map((product)=>{ return <div key={product.id} >{product.title}</div>})}
+  return (
+    <div>
+      This is the products page.
+      {filteredProducts.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
     </div>
-};
+  );
+}
