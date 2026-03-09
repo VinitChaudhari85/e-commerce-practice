d976a3d51727f1f3555a0d524b58c23e9611dc9c
Vinit <chaudhari7876@gmail.com>
2026-03-09 15:36:06 +0530

    minor change
    minor change

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,7 @@ export default function Orders() {
     return(
         <div>
             {/* Three will be sum text which shows "Your orders:" and then we will show list of orders  */}
+            
         </div>
     )
 };
